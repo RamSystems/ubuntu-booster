0378a8a8b396f5050b2fdc3b95151f04d94c92b3
mRsUrN <90952894+mRsUrA@users.noreply.github.com>
2021-10-10 05:08:04 +0200

    Update and rename beckup.sh to backup.sh

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+    mkdir /home/backup_ubuntu_super_booster/
+
+    sudo cp  '/etc/sysctl.conf' '/home/backup_ubuntu_super_booster/'
+    sudo cp '/etc/default/grub' '/home/backup_ubuntu_super_booster/'
+    sudo cp '/etc/hdparm.conf' '/home/backup_ubuntu_super_booster/'
+
+
+
+
+
