cbe5b6619fe417ef83e5301560d1bb7403608634
RamSystems <90952894+RamSystems@users.noreply.github.com>
2021-11-28 04:57:59 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-    mkdir /home/backup_ubuntu_super_booster/
+    sudo mkdir /home/backup_ubuntu_super_booster/
 
     sudo cp  '/etc/sysctl.conf' '/home/backup_ubuntu_super_booster/'
     sudo cp '/etc/default/grub' '/home/backup_ubuntu_super_booster/'
     sudo cp '/etc/hdparm.conf' '/home/backup_ubuntu_super_booster/'
+    sudo cp '/etc/fstab' '/home/backup_ubuntu_super_booster/'
 
 
 
